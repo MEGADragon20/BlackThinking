4531a992ca36b0b7b74170edee963fe5c3967925
MEGADragon2.0 <fernandocormar@icloud.com>
2026-01-26 21:17:13 +0100

    local redis

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 # run app.py after starting venv in terminal
 import os
-
+os.system("sudo docker rm -f redis-local")
 os.system("sudo docker run --name redis-local -p 6379:6379 -d redis")
 os.system("source .venv/bin/activate")
 os.system("python app.py")
